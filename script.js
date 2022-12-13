42e8a8aac249efba57801593aaae4c37d167b22b
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-12-13 19:15:25 -0300

    finaliza estilização

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const button = document.getElementById('criar-carta');
 const paragraph = document.getElementById('carta-gerada');
 const input = document.getElementById('carta-texto');
 
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotation = ['rotateleft', 'rotateright'];
+const inclination = ['skewleft', 'skewright'];
+
 button.addEventListener('click', () => {
   paragraph.innerText = '';
 
@@ -13,6 +18,27 @@ button.addEventListener('click', () => {
   const arrayWords = inputWords.split(' ');
 
   for (let i = 0; i < arrayWords.length; i += 1) {
-    paragraph.innerHTML += `<span>${arrayWords[i]}</span>`;
+    const styleRand = Math.floor(Math.random() * style.length);
+    const sizeRand = Math.floor(Math.random() * size.length);
+    const rotationRand = Math.floor(Math.random() * rotation.length);
+    const inclinationRand = Math.floor(Math.random() * inclination.length);
+    paragraph.innerHTML += `<span id="word-${i + 1}" class="${style[styleRand]} ${size[sizeRand]} 
+      ${rotation[rotationRand]} ${inclination[inclinationRand]}">${arrayWords[i]}</span>`;
   }
+  input.value = '';
+});
+
+paragraph.addEventListener('click', (e) => {
+  const styleRand = Math.floor(Math.random() * style.length);
+  const sizeRand = Math.floor(Math.random() * size.length);
+  const rotationRand = Math.floor(Math.random() * rotation.length);
+  const inclinationRand = Math.floor(Math.random() * inclination.length);
+
+  e.target.setAttribute('class', '');
+  e.target.classList.add(
+    style[styleRand],
+    size[sizeRand],
+    rotation[rotationRand],
+    inclination[inclinationRand],
+  );
 });
